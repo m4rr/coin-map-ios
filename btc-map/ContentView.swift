eaf87fe7057766f4f1c3791579cc7a7c185718c5
Marat Saytakov <remarr@gmail.com>
2019-11-16 18:44:20 +0700

    Initial Commit

---
+++ btc-map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  btc-map
+//
+//  Created by Marat Saytakov on 16.11.2019.
+//  Copyright © 2019 saytakov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
