1a959f5b0f51f070f3c905fed216a611b7220c41
Marat Saytakov <remarr@gmail.com>
2019-11-27 01:25:47 +0700

    fix safeAreas for not-home-bar devices

--- coin-map/ContentView.swift
+++ coin-map/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
 
   private var detailsIsNotEmpty: Bool {
     return !selectedCurrency.isEmpty || [
-        selectedSubtitle, selectedPhone, selectedWebsite, selectedHours,
+      selectedSubtitle, selectedPhone, selectedWebsite, selectedHours,
       ]
       .reduce(false, { $0 || !$1.isEmpty })
   }
@@ -29,138 +29,159 @@ struct ContentView: View {
   @State private var alert1: Bool = false
   @State private var alert2: Bool = false
 
-  var edgeSet: Edge.Set {
-    #if targetEnvironment(macCatalyst)
-    return .all
-    #else
-    return [.top, .leading, .trailing]
-    #endif
+  func paddingEdges(insets: EdgeInsets) -> Edge.Set {
+    if insets.bottom.isZero {
+      return .all
+    } else {
+      return [.top, .leading, .trailing]
+    }
   }
 
+//  var paddingEdgeSet: Edge.Set {
+//    #if targetEnvironment(macCatalyst)
+//    return .all
+//    #else
+//    if UIView().safeAreaInsets.bottom.isZero {
+//      return .all
+//    } else {
+//      return [.top, .leading, .trailing]
+//    }
+//    #endif
+//  }
+
   var body: some View {
-    ZStack {
-      MapRepresentable(selected: $selected,
-                       title: $selectedTitle,
-                       subtitle: $selectedSubtitle,
-                       phone: $selectedPhone,
-                       website: $selectedWebsite,
-                       hours: $selectedHours,
-                       placeCurrencies: $selectedCurrency)
-        .edgesIgnoringSafeArea(.all)
+    GeometryReader { g in
+      ZStack {
+        MapRepresentable(selected: self.$selected,
+                         title: self.$selectedTitle,
+                         subtitle: self.$selectedSubtitle,
+                         phone: self.$selectedPhone,
+                         website: self.$selectedWebsite,
+                         hours: self.$selectedHours,
+                         placeCurrencies: self.$selectedCurrency)
+          .edgesIgnoringSafeArea(.all)
 
-      VStack {
-        HStack {
-          Spacer()
+        VStack {
+          HStack {
+            Spacer()
 
-          Button(action: {
-            //
-          }) {
-            Text("i")
-              .font(.system(.headline, design: .monospaced))
-              .foregroundColor(.white)
+            Button(action: {
+              //
+            }) {
+              Text("i")
+                .font(.system(.headline, design: .monospaced))
+                .foregroundColor(.white)
+            }
+            .frame(width: 40, height: 40)
+            .background(LinearGradient(gradient: Gradient(colors: [.red, .blue]),
+                                       startPoint: .leading, endPoint: .trailing))
+              .cornerRadius(40)
           }
-          .frame(width: 40, height: 40)
-          .background(LinearGradient(gradient: Gradient(colors: [.red, .blue]),
-                                     startPoint: .leading, endPoint: .trailing))
-          .cornerRadius(40)
-        }
 
 
-        Spacer() // here we see the map
+          Spacer() // here we see the map
 
-        if selected {
-          VStack(spacing: 8) {
+          if self.selected {
+            VStack(spacing: 8) {
 
-            if !selectedTitle.isEmpty {
-              Text(selectedTitle)
-                .font(.system(.headline, design: .rounded))
-                .multilineTextAlignment(.center)
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .foregroundColor(Color(UIColor.label))
-            }
+              if !self.selectedTitle.isEmpty {
+                Text(self.selectedTitle)
+                  .font(.system(.headline, design: .rounded))
+                  .multilineTextAlignment(.center)
+                  .frame(minWidth: 0, maxWidth: .infinity)
+                  .foregroundColor(Color(UIColor.label))
+              }
 
-            if !selectedTitle.isEmpty && detailsIsNotEmpty {
-              Divider()
-            }
+              if !self.selectedTitle.isEmpty && self.detailsIsNotEmpty {
+                Divider()
+              }
 
-            if !selectedCurrency.isEmpty {
-              Text(selectedCurrency.description)
-                .font(.system(.footnote, design: .monospaced))
-            }
+              if !self.selectedCurrency.isEmpty {
+                Text(self.selectedCurrency.description)
+                  .font(.system(.footnote, design: .monospaced))
+              }
 
-            if !selectedSubtitle.isEmpty {
-              Text(selectedSubtitle)
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .font(.subheadline)
-                .foregroundColor(Color(UIColor.label))
-            }
+              if !self.selectedSubtitle.isEmpty {
+                Text(self.selectedSubtitle)
+                  .frame(minWidth: 0, maxWidth: .infinity)
+                  .font(.subheadline)
+                  .foregroundColor(Color(UIColor.label))
+              }
 
-            if !selectedPhone.isEmpty {
-              Button(action: {
-                self.alert1.toggle()
-              }, label: {
-                Text(selectedPhone)
-              })
-                .actionSheet(isPresented: $alert1) {
-                  ActionSheet(
-                    title: Text("Call Number"),
-                    message: Text(self.selectedPhone),
-                    buttons: [
-                      Alert.Button.default(Text("Call")) {
-                        if let phone = URL(string: "tel:" + self.selectedPhone.trimmingCharacters(in: .whitespaces)) {
-                          UIApplication.shared.open(phone, options: [:], completionHandler: nil)
-                        }
-                      },
-                      Alert.Button.default(Text("Copy")) {
-                        UIPasteboard.general.string = self.selectedPhone
-                      },
-                      Alert.Button.cancel(),
-                    ]
-                  )
+              if !self.selectedPhone.isEmpty {
+                Button(action: {
+                  self.alert1.toggle()
+                }, label: {
+                  Text(self.selectedPhone)
+                })
+
+                  .popSheet(isPresented: self.$alert1, arrowEdge: .bottom, content: { () -> PopSheet in
+
+
+                    PopSheet(title: Text("Call Number"), message: Text(self.selectedPhone), buttons: <#T##[PopSheet.Button]#>)
+
+
+//                  .actionSheet(isPresented: self.$alert1) {
+                    ActionSheet(
+                      title: ,
+                      message: ,
+                      buttons: [
+                        Alert.Button.default(Text("Call")) {
+                          if let phone = URL(string: "tel:" + self.selectedPhone.trimmingCharacters(in: .whitespaces)) {
+                            UIApplication.shared.open(phone, options: [:], completionHandler: nil)
+                          }
+                        },
+                        Alert.Button.default(Text("Copy")) {
+                          UIPasteboard.general.string = self.selectedPhone
+                        },
+                        Alert.Button.cancel(),
+                      ]
+                    )
+                }
+              }
+
+              if !self.selectedWebsite.isEmpty {
+                Button(action: {
+                  self.alert2.toggle()
+                }, label: {
+                  Text(self.selectedWebsite)
+                })
+                  .actionSheet(isPresented: self.$alert2) {
+                    ActionSheet(
+                      title: Text("Open In Safari"),
+                      message: Text(self.selectedWebsite),
+                      buttons: [
+                        Alert.Button.default(Text("Open"), action: {
+                          if let url = URL(string: self.selectedWebsite) {
+                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                          }
+                        }),
+                        Alert.Button.default(Text("Copy")) {
+                          UIPasteboard.general.string = self.selectedWebsite
+                        },
+                        Alert.Button.cancel(),
+                      ]
+                    )
+                }
+              }
+
+              if !self.selectedHours.isEmpty {
+                Text(self.selectedHours)
+                  .foregroundColor(Color(UIColor.label))
+
               }
             }
-
-            if !selectedWebsite.isEmpty {
-              Button(action: {
-                self.alert2.toggle()
-              }, label: {
-                Text(selectedWebsite)
-              })
-                .actionSheet(isPresented: $alert2) {
-                  ActionSheet(
-                    title: Text("Open In Safari"),
-                    message: Text(self.selectedWebsite),
-                    buttons: [
-                      Alert.Button.default(Text("Open"), action: {
-                        if let url = URL(string: self.selectedWebsite) {
-                          UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                        }
-                      }),
-                      Alert.Button.default(Text("Copy")) {
-                        UIPasteboard.general.string = self.selectedWebsite
-                      },
-                      Alert.Button.cancel(),
-                    ]
-                  )
-              }
-            }
-
-            if !selectedHours.isEmpty {
-              Text(selectedHours)
-                .foregroundColor(Color(UIColor.label))
-
-            }
-          }
-          .padding()
-          .background(Color(UIColor.systemBackground))
-          .cornerRadius(10)
-          .transition(AnyTransition
+            .padding()
+            .background(Color(UIColor.systemBackground))
+            .cornerRadius(10)
+            .transition(AnyTransition
             .opacity
             .animation(.easeOut(duration: 0.2))
-          )
+            )
+          }
         }
+        .padding(self.paddingEdges(insets: g.safeAreaInsets))
       }
-      .padding(edgeSet)
     }
   }
 }
